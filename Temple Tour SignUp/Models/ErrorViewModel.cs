293234d25c2027e4b897f8b273555022a3a4dc78
jessi472 <79349080+jessi472@users.noreply.github.com>
2022-03-16 09:07:19 -0600

    Add project files.

---
+++ Temple Tour SignUp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Temple_Tour_SignUp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
