0d792207a0d97447c5607f0630cad467ec389cf7
AliciaLane01 <62069491+AliciaLane01@users.noreply.github.com>
2022-03-21 17:32:06 -0600

    Started Appointment model

---
+++ Temple Tour SignUp/Models/Appointment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Temple_Tour_SignUp.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [Required]
+        int AppointmentId { get; set; }
+        //link to time slot model like we did with categories
+        [Required]
+        string GroupName { get; set; }
+        [Required]
+        [Range(1, 15)] //(Max of 15)
+        int GroupSize { get; set; }
+        [Required]
+        string Email { get; set; }
+        string Phone { get; set; }
+
+        //Foreign Key Relationship
+        [Required]
+        int TimeSlotId { get; set; }
+        public TimeSlot TimeSlot { get; set; }
+    }
+}
---
+++ Temple Tour SignUp/Models/SignUpContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Temple_Tour_SignUp.Models
+{
+    public class SignUpContext : DbContext
+    {
+        public SignUpContext()
+        {
+        }
+
+        public SignUpContext(DbContextOptions<SignUpContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
