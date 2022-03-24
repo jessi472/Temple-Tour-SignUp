d810aa1edad739507d46fe8907a42018d05cc2b2
AliciaLane01 <62069491+AliciaLane01@users.noreply.github.com>
2022-03-23 20:38:11 -0600

    fix what I broke?

--- Temple Tour SignUp/wwwroot/js/site.js
+++ Temple Tour SignUp/wwwroot/js/site.js
@@ -7,9 +7,9 @@
 
 //Modal for pop up edit option
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+//var myModal = document.getElementById('myModal')
+//var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
-})
+//myModal.addEventListener('shown.bs.modal', function () {
+//    myInput.focus()
+//})
